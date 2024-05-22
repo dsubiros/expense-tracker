36bba74b9bca300971bf5ffded892d96559b83ae
Dariel Subiros <dsubiros@gmail.com>
2024-05-22 16:55:54 -0400

    Deleting data

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
+import Spinner from "./components/Spinner";
 
 const url = "https://jsonplaceholder.typicode.com/users1";
 
@@ -11,27 +12,62 @@ interface User {
 const App2 = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     const fetchData = async () => {
       try {
-        const { data } = await axios.get<User[]>(url);
+        setIsLoading(true);
+        const { data } = await axios.get<User[]>(url, {
+          signal: controller.signal,
+        });
+
         setUsers(data);
+        setIsLoading(false);
       } catch (error) {
+        if (error instanceof CanceledError) return;
         setError((error as AxiosError).message);
+        setIsLoading(false);
       }
     };
 
     fetchData();
+
+    return () => controller.abort();
   }, []);
+
+  const deleteUser = (id: number) => {
+    const originalUsers = [...users];
+
+    setUsers(users.filter((u) => u.id !== id));
+    axios.delete(`${url}/${id}`)
+    .catch((err: AxiosError) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <>
+      {isLoading && <Spinner />}
       {error && <p className="text-danger">ERROR: {error}</p>}
 
-      <ul>
+      <ul className="list-group">
         {users.map(({ id, name }) => (
-          <li key={id}>{name}</li>
+          <li
+            key={id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {name}{" "}
+            <button
+              onClick={() => deleteUser(id)}
+              className="btn btn-sm btn-outline-danger"
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div><div className="spinner-border"></div> Loading...</div>
+  )
+}
+
+export default Spinner
